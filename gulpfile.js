991a497ea28c24f7d76b7e429eb14ceb9c66178a
Anthony Ramella <contact@anthonyramella.com>
2017-02-28 16:18:05 -0500

    update directory structure, add breakpoint support

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
 var src = {
-    scss: 'app/scss/*.scss',
-    css:  'app/css',
-    html: 'app/*.html'
+    scss: 'src/scss/*.scss',
+    css:  'src/css',
+    html: 'src/*.html'
 };
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./app"
+        server: "./src"
     });
 
     gulp.watch(src.scss, ['sass']);
@@ -22,7 +22,8 @@ gulp.task('serve', ['sass'], function() {
 // Compile sass into CSS
 gulp.task('sass', function() {
     return gulp.src(src.scss)
-        .pipe(sass())
+        .pipe(sass({ includePaths: ['./node_modules/breakpoint-sass/stylesheets']
+            }))
         .pipe(gulp.dest(src.css))
         .pipe(browserSync.reload({stream: true}));
 });
